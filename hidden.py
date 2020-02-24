216e325d080d50f27e3c5700231b7bff9575d080
Daria Omelkina <odasha2001@gmail.com>
2020-02-24 17:57:33 +0200

    Security

--- hidden.py
+++ hidden.py
@@ -1,5 +1,6 @@
+# These keys are broken because of security issues.
 def oauth():
-    return {"consumer_key": "lnZcQncNUUlKVRU7dxVs7isiu",
-            "consumer_secret": "bhhcsK4UyePOgJ0qq9uSW0yrWWs9vX6fHajQfQVV9Mq33Y9gsp",
-            "token_key": "1230454841916493826-jAncAwy8loC112RQtV2b8yinYlECj9",
-            "token_secret": "rfp2VeGlsOKsh1HkgDN69VkZ8P7c5mP14xKka0VoznjN3"}
+    return {"consumer_key": "lnZc",
+            "consumer_secret": "bhhcsK",
+            "token_key": "123",
+            "token_secret": "rfp"}
